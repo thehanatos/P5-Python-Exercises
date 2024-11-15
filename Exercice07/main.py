7ae2f784156e074e7c14aad4ca85830b3c5d76e3
thehanatos <admin4523@gmail.com>
2024-11-15 16:50:35 +0100

    exercise solved

--- Exercice07/main.py
+++ Exercice07/main.py
@@ -1,1 +1,25 @@
-## Écrivez votre code ici !
+# Écrivez votre code ici !
+def square(nb):
+    """
+    Calculate the square of a given number.
+
+    Args:
+        nb (int or float): number
+
+    Returns:
+        int or float: returns the square of the number
+    """
+    if isinstance(nb, str) is True:
+        print("Le paramètre doit être un nombre !")
+        return None
+    else:
+        result = nb * nb
+        return print(result)
+
+
+# Examples of use :
+square(3)
+
+square(3.3)
+
+square('ab')
