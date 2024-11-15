1b2f82854743033d633b7ada51f3e3b0a17179d9
thehanatos <admin4523@gmail.com>
2024-11-15 16:32:06 +0100

    exercise solved

--- Exercice06/main.py
+++ Exercice06/main.py
@@ -1,5 +1,11 @@
 # Fonction calculate_average
- 
+def calculate_average(list):
+    sum = 0
+    for number in list:
+        sum = sum + number
+    return sum / len(list)
+
+
 # Exemple d'utilisation de la fonction
 numbers = [10, 20, 30, 40, 50]
 average = calculate_average(numbers)
