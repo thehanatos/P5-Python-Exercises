ae482e39e434bab6122b016a5a87839a03bf70c4
thehanatos <admin4523@gmail.com>
2024-11-15 15:54:59 +0100

    exercise resolved

--- Exercice03/main.py
+++ Exercice03/main.py
@@ -1,1 +1,18 @@
 words = ["python", "programmation", "langage", "ordinateur", "apprentissage"]
+voyels = ["a", "e", "i", "o", "u", "y"]
+voyel_number = 0
+words_with_voyels = []
+
+
+def count_voyels(word):
+    voyel_number = 0
+    for voyel in word:
+        if voyel in voyels:
+            voyel_number += 1
+    return word, voyel_number
+
+
+for word in words:
+    words_with_voyels.append((count_voyels(word)))
+
+print(words_with_voyels)
