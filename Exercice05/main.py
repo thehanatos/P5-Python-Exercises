c381790c0a846a144e95c03aa29164d14e9da5ac
thehanatos <admin4523@gmail.com>
2024-11-15 16:09:40 +0100

    exercise solved

--- Exercice05/main.py
+++ Exercice05/main.py
@@ -1,5 +1,26 @@
 def sum(a, b):
+    """
+    Calculate the sum of two numbers
+
+    Args:
+        a (int or float): first number
+        b (int or float): second number
+
+    Returns:
+        int or float: result of the sum a + b
+    """
     return a + b
- 
+
+
 def subtraction(a, b):
+    """
+    Calculate the substraction of two numbers
+
+    Args:
+        a (int or float): first number
+        b (int or float): second number
+
+    Returns:
+        int or float: result of the substraction a - b
+    """
     return a - b
