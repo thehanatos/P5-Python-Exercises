3f5095aceb6e79849510b10766cbe900a6d1b2a1
thehanatos <admin4523@gmail.com>
2024-11-15 18:19:46 +0100

    exeercise solved

--- Exercice10/main.py
+++ Exercice10/main.py
@@ -1,1 +1,24 @@
-## Écrivez votre code ici !
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display_details(self):
+        print(f"The person is {self.name}, {self.age} years old")
+
+
+class Employee(Person):
+    def __init__(self, name, age, salary):
+        super().__init__(name, age)
+        self.salary = salary
+
+    def display_details(self):
+        super().display_details()
+        print(f"The salary is {self.salary}")
+
+
+new_person = Person("Carrie", 17)
+new_person.display_details()
+
+new_employee = Employee("Eva", 28, 35000)
+new_employee.display_details()
