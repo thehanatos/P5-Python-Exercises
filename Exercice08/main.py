b2768e945d9f6d8387a6c0196880bcd8f282c1f3
thehanatos <admin4523@gmail.com>
2024-11-19 09:53:14 +0100

    exercise solved

--- Exercice08/main.py
+++ Exercice08/main.py
@@ -1,8 +1,18 @@
 def log_decorator(func):
-     pass
- 
+    """
+    A decorator that logs messages before and after the execution of the given
+    function. Works only with functions without arguments.
+    """
+    def wrapper():
+        print(f"Starting execution of {func.__name__}...")
+        func()  # Call the original function
+        print(f"Finished execution of {func.__name__}.")
+    return wrapper
+
+
 @log_decorator
 def function_test():
     print("Cette fonction ne prend pas d'arguments.")
+
 
 function_test()
